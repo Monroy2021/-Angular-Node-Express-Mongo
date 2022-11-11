7779c8c2341ebaacfb60a793eccebe37752ca150
Monroy2021 <luismonroy1014@gmail.com>
2022-11-11 10:17:32 -0500

    Se realizo la parte Back con sus carpetas config,controllers, models, routers y la conexion a mongo

--- cliente/src/app/app.module.ts
+++ cliente/src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser'; 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
@@ -14,14 +16,15 @@ import { ListarProductosComponent } from './components/listar-productos/listar-p
     AppComponent,
     CrearProductoComponent,
     ListarProductosComponent
-    
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule, 
-    BrowserAnimationsModule, 
-     ToastrModule.forRoot()
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
 
   ],
   providers: [],
